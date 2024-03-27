cc49ad11951021b00a46223e6b969febe4231c30
casaer01 <ecasas182015@gmail.com>
2024-03-26 21:34:01 -0500

    added documentation link & method

--- POSstart.py
+++ POSstart.py
@@ -38,7 +38,6 @@ job = win32print.StartDocPrinter(p, 1, ("Test document", None, "raw"))
 win32print.StartPagePrinter(p)
 
 
-
 # Write data to the printer
 # Replace "data to print" with the actual data you want to print
 
@@ -50,3 +49,23 @@ win32print.EndDocPrinter(p)
 
 # Close the printer
 win32print.ClosePrinter(p)
+
+
+# third method
+
+printer_name = "Printer Name"
+printer_handle = win32print.OpenPrinter(printer_name)
+# Set the printer properties
+properties = win32print.GetPrinter(printer_handle, 2)
+properties['pDevMode'].Duplex = win32print.DMDUP_SIMPLEX  # Set duplex mode to simplex
+properties['pDevMode'].Copies = 2  # Set number of copies to 2
+win32print.SetPrinter(printer_handle, 2, properties, 0)
+# Print a document
+job_info = ('Test Document', None, {'DesiredAccess': win32print.PRINTER_ALL_ACCESS})
+job_handle = win32print.StartDocPrinter(printer_handle, 1, job_info)
+win32print.StartPagePrinter(printer_handle)
+win32print.WritePrinter(printer_handle, b'This is a test document')
+win32print.EndPagePrinter(printer_handle)
+win32print.EndDocPrinter(printer_handle)
+# Close the printer handle
+win32print.ClosePrinter(printer_handle)
--- Readme.md
+++ Readme.md
@@ -3,4 +3,8 @@ The purpose is to start developing a POS system for resturant use.
 ##To Do
 
 - create a way to print to kitchen printer
-    - select items from menu then allow it to be printed.
+  - select items from menu then allow it to be printed.
+
+Documentation
+
+https://timgolden.me.uk/pywin32-docs/win32print.html
